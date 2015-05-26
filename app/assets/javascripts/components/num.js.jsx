a78da3e2896c941c3a4799d08562306fc87ef524
Dylan Richards <dylanerichards@gmail.com>
2015-05-26 12:48:23 -0400

    Random number component

--- app/assets/javascripts/components/num.js.jsx
+++ app/assets/javascripts/components/num.js.jsx
@@ -1,9 +1,14 @@
-
 var Num = React.createClass({
   getInitialState: function() {
     return {
       value: this.props.value
     };
+  },
+
+  handleClick: function() {
+    this.setState ({
+      value: Math.floor(Math.random() * 100) + 1
+    });
   },
 
   render: function() {
